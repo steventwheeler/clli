e560ebdc5afdf5ecc4f8aa65256ca98b843a788c
Steven Wheeler <steventwheeler@gmail.com>
2016-01-14 23:32:34 -0600

    Reduced complexity of entity_type.rb
    Also bumped version to v0.0.2.

--- lib/clli/entity_type.rb
+++ lib/clli/entity_type.rb
@@ -29,26 +29,32 @@ class CLLI
 
       private
 
+      SWITCHING_TYPE_PATTERNS = {
+        "MG[#{x1}]" => %w(switching_types MGx),
+        "SG[#{x1}]" => %w(switching_types SGx),
+        "CG[#{x1}]" => %w(switching_types CGx),
+        "DS[#{x1}]" => %w(switching_types DSx),
+        "[#{n}]{2}[#{x1}]" => %w(switching_types nnx),
+        "[#{n}]{2}T" => %w(switching_types nnT),
+        "C[#{n}]T" => %w(switching_types CnT),
+        "B[#{n}]T" => %w(switching_types BnT),
+        "[#{n}]GT" => %w(switching_types nGT),
+        "Z[#{a}]Z" => %w(switching_types ZaZ),
+        "RS[#{n}]" => %w(switching_types RSn),
+        "X[#{a}]X" => %w(switching_types XaX),
+        "CT[#{x1}]" => %w(switching_types CTx)
+      }
+
       ##
       # Determine which key to use for the parsed entity code.
       #
       # Params:
       # +code+:: the +CLLI+ +:entity_code+ attribute value.
       def code_keys(code)
+        SWITCHING_TYPE_PATTERNS.each do |pattern, keys|
+          return keys if Regexp.new(pattern) =~ code
+        end
         case code
-        when Regexp.new("MG[#{x1}]") then %w(switching_types MGx)
-        when Regexp.new("SG[#{x1}]") then %w(switching_types SGx)
-        when Regexp.new("CG[#{x1}]") then %w(switching_types CGx)
-        when Regexp.new("DS[#{x1}]") then %w(switching_types DSx)
-        when Regexp.new("[#{n}]{2}[#{x1}]") then %w(switching_types nnx)
-        when Regexp.new("[#{n}]{2}T") then %w(switching_types nnT)
-        when Regexp.new("C[#{n}]T") then %w(switching_types CnT)
-        when Regexp.new("B[#{n}]T") then %w(switching_types BnT)
-        when Regexp.new("[#{n}]GT") then %w(switching_types nGT)
-        when Regexp.new("Z[#{a}]Z") then %w(switching_types ZaZ)
-        when Regexp.new("RS[#{n}]") then %w(switching_types RSn)
-        when Regexp.new("X[#{a}]X") then %w(switching_types XaX)
-        when Regexp.new("CT[#{x1}]") then %w(switching_types CTx)
         when Regexp.new(switchboard_and_desk_entity_code_pattern) then ['switchboard_and_desk', code[1]]
         when Regexp.new(miscellaneous_switching_termination_entity_code_pattern) then ['miscellaneous_switching_termination', code[1]]
         when Regexp.new(nonswitching_entity_code_pattern) then ['nonswitching_types', code[0]]
--- lib/clli/version.rb
+++ lib/clli/version.rb
@@ -1,4 +1,4 @@
 # This file defines the gem version.
 class CLLI
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
 end
